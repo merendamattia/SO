a155ea6ad5047fa16bc870337c1c0b5e4bff54fb
0xMere <merenda.s.mattia@gmail.com>
2022-12-07 19:53:03 +0100

    Update thread.c

--- progetto/thread.c
+++ progetto/thread.c
@@ -8,56 +8,71 @@
 pthread_mutex_t mutex;
 int sum = 0;
 double total_time_taken = 0;
+int global_quoziente = 0;
+
+int sumInMain(int vet[], int dim_array){
+	int sum_in_main = 0;
+	for(int i = 0; i < dim_array; i++)
+		sum_in_main += vet[i];
+	return sum_in_main;
+}
+
+void populateArrayRandom(int vet[], int dim_array){
+	srand(time(NULL));
+	for(int i = 0; i < dim_array; i++)
+		vet[i] = rand() % MAX_LIMIT + 1;
+}
+
+void outputArray(int vet[], int dim_array){
+	for(int i = 0; i < dim_array; i++)
+		if(vet[i] != 0)
+			printf("%d) %d\n", i + 1, vet[i]);
+}
 
 double getTimeTaken(clock_t t){
 	t = clock() - t;
   	return ( (double)t ) / CLOCKS_PER_SEC;
 }
 
-int something_worked(void) {
+int isWorking(void) {
     /* thread operation might fail, so here's a silly example */
-	void *p = malloc(100);
-	free(p);
-	return p ? 1 : 0;
+	void *tmp = malloc(5);
+	free(tmp);
+	return tmp ? 1 : 0;
 }
 
-void *myThread(int *arg) {
+void *myThread(int arg[]) {
 	clock_t t = clock();
 
-	if (something_worked()) {
-		
-		printf("Hello world! I'm thread%d\n", ((int)arg + 1));
-		fflush(stdout);
+	if (isWorking()) {
 
-		sum++;
-		
+		int internal_sum = 0;
+		for(int i = 0; i < global_quoziente; i++)
+			internal_sum += arg[i];
+
 		pthread_mutex_lock(&mutex); // entra nella zona critica
+		//printf("+++\tAccesso zona critica tid%d\n", pthread_self());
 		
-		printf("+++\tAccesso zona critica td%d\n", ((int)arg + 1));
+		sum += internal_sum;
 
 		float time_taken = getTimeTaken(t);
-		printf("Thread%d took %f sec to execute \n", ((int)arg + 1), time_taken);
+
+		printf("\ntid_%d exec time %f sec, internal_sum:%d \n", pthread_self(), time_taken, internal_sum);
+		//printf("indirizzo array:%d\n", &arg);
 		fflush(stdout);
+		
 		total_time_taken += time_taken;
 
-		printf("---\tRilascio zona critica td%d\n", ((int)arg + 1));
-
+		//printf("---\tRilascio zona critica tid: %d\n", pthread_self());
 		pthread_mutex_unlock(&mutex); // esce dalla zona critica
+
+		// printf("tid%d contenuto array\n", pthread_self());
+		// outputArray(arg, global_quoziente);
+		// printf("\n");
 
 		pthread_exit(1);
 	} 
 	else pthread_exit(0);
-}
-
-void populateArrayRandom(int vet[], int dim_array){
-	srand(time(NULL));
-	for(int i = 0; i < dim_array; i++)
-		vet[i] = rand() % MAX_LIMIT;
-}
-
-void outputArray(int vet[], int dim_array){
-	for(int i = 0; i < dim_array; i++)
-		printf("%d\n", vet[i]);
 }
 
 int main() {
@@ -80,9 +95,10 @@ int main() {
 	    
 	//int vet[dim_array];
 
-	// ---------------- Popolamento array con valori casuali che vanno da 0 a MAX_LIMIT
+	// ---------------- Popolamento array con valori casuali che vanno da 1 a MAX_LIMIT + 1
 	populateArrayRandom(vet, dim_array); 
-	outputArray(vet, dim_array);
+	// printf("Output array:\n");
+	// outputArray(vet, dim_array);
 
 	// ---------------- Input numero thread
 	do {
@@ -97,10 +113,39 @@ int main() {
 	pthread_t threads[dim_thread]; 
 	void *status = 0; 
 
+	/* Usando questa formula avremo l'ultimo thread con meno numeri
+	* Dim array: 21
+	* Num thread: 6
+	* Crea 6 array 
+	* 5 array hanno 4 elementi
+	* 1 array ha 1 elemento
+	*/
+	int quoziente = dim_array / (dim_thread - 1);
+	printf("\nQuoziente: %d\n", dim_array / dim_thread);
+
+	global_quoziente = quoziente;
+
+	int modulo = dim_array % dim_thread;
+	printf("Modulo (resto): %d\n\n", modulo);
+
 	// ---------------- Creazione thread ed esecuzione
 	for(int i = 0; i < dim_thread; i++){
+
+		int *new_array = (int *)malloc(quoziente * sizeof (int));
 		
-		pthread_create(&threads[i], NULL, myThread, i);
+		for(int j = 0; j < quoziente; j++){
+			
+			new_array[j] = vet[quoziente * i + j];
+
+			if(i + 1 != dim_thread && j >= quoziente)
+				new_array[j] = 0;
+		}
+	
+		pthread_create(&threads[i], NULL, myThread, new_array);
+
+		sleep(1); // senza questo non funziona, TODO: da sistemare
+		free(new_array); 
+		new_array = NULL;
 	}
 	
 	// ---------------- Join dei thread
@@ -110,14 +155,22 @@ int main() {
 		if(status == 0) printf("Error thread%d", i);
 	}
 
-	printf("Sum: %d\n", sum);
 
-	printf("Threads exec time %f sec \n", total_time_taken);
+	printf("\nThreads exec time %f sec \n", total_time_taken);
 	printf("Threads exec (with creation) time %f sec \n", getTimeTaken(total_time_taken_included_creation));
+
+	pthread_mutex_lock(&mutex);
+	printf("\nSum thread: %d\n", sum);
+	pthread_mutex_unlock(&mutex);
+
+	clock_t total_time_taken_in_main = clock();
+	printf("Sum in main: %d\n", sumInMain(vet, dim_array));
+	printf("Sum in main exec time %f sec \n", getTimeTaken(total_time_taken_in_main));
 
 	pthread_mutex_destroy(&mutex); // distruzione del mutex
 	free(vet);  // Abbiamo finito di lavorare con l'array, e liberiamo il puntatore associato
 	vet = NULL; // Il puntatore non può essere più usato fino ad un riassegnamento effettuato con malloc. */
-	
+	// free(new_array); 
+	// new_array = NULL;
   	return 0;
 }
