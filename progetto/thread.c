6a4c94e2538f413ee841b468f2626d80a2791729
0xMere <merenda.s.mattia@gmail.com>
2022-12-11 18:44:21 +0100

    v1.1 threads

--- progetto/thread.c
+++ progetto/thread.c
@@ -1,17 +1,12 @@
 #include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include "utility.c"
- 
+#include "./utility.c"
+    
 // Variabili globali
-pthread_mutex_t mutex;
+sem_t mutex;
 int t_sum = 0;
 
 struct arg_struct {
   int *vet;
-  int tid;
   int start;
   int finish;
 }; 
@@ -34,13 +29,11 @@ void *myThread(struct arg_struct* arguments) {
 		for(int i = args->start; i < args->finish; i++)
 			internal_sum += args->vet[i];
 
-		pthread_mutex_lock(&mutex); // entra nella zona critica
-		//printf("+++\tAccesso zona critica tid%d\n", pthread_self());
+		sem_wait(&mutex);
 		
 		t_sum += internal_sum;
 
-		//printf("---\tRilascio zona critica tid: %d\n", pthread_self());
-		pthread_mutex_unlock(&mutex); // esce dalla zona critica
+		sem_post(&mutex);
 
 		// printf("\ntid_%d", pthread_self());
 		// //printf("\n\texec time %f sec", time_taken);
@@ -61,25 +54,30 @@ void *myThread(struct arg_struct* arguments) {
 }
 
 int main() {
-	printf("Merenda Saverio Mattia [MAT: 330503]\n\n");
-	
-	int dim_array, dim_thread;
-	pthread_mutex_init (&mutex, NULL); // Il mutex è iniziamente libero
-	
-	dim_array = getDimArray(); // Input dimensione array
+	printf("Merenda Saverio Mattia [MAT: 330503]\n");
+	printf("Threads\n\n");
+
+	sem_init(&mutex, 0, 1);	
+
+	int dim_array = getDimArray(); // Input dimensione array
+	int dim_thread = getNumThreads(); // Input numero thread
 
 	int *vet = (int *) malloc(dim_array * sizeof (int)); // Alloca spazio per l'array
-	struct arg_struct *args = malloc(dim_array * sizeof (struct arg_struct)); // Alloca spazio per la struct
+	struct arg_struct *args = malloc(dim_thread * sizeof (struct arg_struct)); // Alloca spazio per la struct
 
 	if (!vet || !args) { 
 		printf("Errore nell'allocazione della memoria :(\n");
 		return -1;
 	}
-
-	populateArrayRandom(vet, dim_array); // Popolamento array con valori casuali che vanno da - MAX_LIMIT/2 A + MAX_LIMIT/2
+	
+	if(dim_array >= 100000000)
+		printf("\n\n***** POPOLAMENTO ARRAY IN CORSO *****\n");
+	
+	clock_t start_array_population_time = clock(); // Start cronometro
+	populateArrayRandom(vet, dim_array); // Popolamento array con valori casuali che vanno da 1 a MAX_LIMIT + 1
+	printf("Array population time %f sec\n", getTimeTaken(start_array_population_time));
+	
 	//outputArray(vet, dim_array);
-
-	dim_thread = getNumThreads(); // Input numero thread
 
 	int status = 0; // Variabile di stato dei threads
 
@@ -91,26 +89,12 @@ int main() {
 	* 1 array ha 1 elemento
 	*/
 	int modulo = dim_array % dim_thread;
-	int quoziente;
+	int quoziente = getQuoziente(modulo, dim_array, dim_thread);
 
-	// printf("\nQuoziente: %d", dim_array / dim_thread);
-	
-	if(modulo == 0)
-		quoziente = dim_array / dim_thread;
-	else 
-		quoziente = dim_array / (dim_thread - 1); // è più ottimizzato
 
-	// printf("\nQuoziente scelto: %d\n", quoziente);
-	// printf("Modulo (resto): %d\n", modulo);
+	printf("\n\n***** LOADING *****\n\n");
 
-	// if(modulo == 0)
-	// 	printf("\n%d thread con %d elementi da sommare\n", dim_thread, quoziente);
-	// else {
-	// 	printf("\n%d thread con %d elementi da sommare\n", dim_thread - 1, quoziente);
-	// 	printf("1 thread con %d elementi da sommare\n", dim_array % (dim_thread - 1));
-	// }
-
-	clock_t start_exec_time_threads = clock(); // Start cronometro
+	clock_t start_exec_time_threads = clock(); 
  
 	pthread_t threads[dim_thread]; // Creazione array di thread
 	
@@ -120,33 +104,21 @@ int main() {
 		// Popolamento della struct che verrà passata come argomento ai vari thread
 		// Inserisco le informazioni generali
 		args[i].vet = vet;
-
-		if(i == 0) {
-			args[i].start = 0;
-			args[i].finish = quoziente;
-		} else if(i + 1 == dim_thread) {
-			args[i].start = quoziente * i;
-			args[i].finish = dim_array;
-		} else {
-			args[i].start = quoziente * i;
-			args[i].finish = quoziente * (i + 1);
-		}
-	
+		args[i].start = getStartIndex(i, quoziente);
+		args[i].finish = getFinishIndex(i, quoziente, dim_thread, dim_array);
+		
 		pthread_create(&threads[i], NULL, myThread, &args[i]);
 	}
 	
 	// ---------------- Detach dei thread
 	for(int i = 0; i < dim_thread; i++) {
 		status = pthread_detach(threads[i]);
-		// pthread_join(threads[i], &status);
 
 		if(status != 0) 
 			printf("Errore nel join con il thread%d\nStatus: %d", i, status);
 	}
 
 	double total_exec_time_threads = getTimeTaken(start_exec_time_threads);
-
-	printf("\n\n***** LOADING *****\n\n");
 
 	clock_t start_exec_time_main = clock();
 	int m_sum = sumInMain(vet, dim_array);
@@ -164,8 +136,8 @@ int main() {
 	printf("Execution time %f sec \n", total_exec_time_threads);
 	
 
-	// Distruzione del mutex
-	pthread_mutex_destroy(&mutex); 
+	// Distruzione del semaforo
+	sem_destroy(&mutex);
 
 	// Deallocazione memoria dinamica utilizzata
 	free(vet);  
